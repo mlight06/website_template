f9c69fbfaaef5833a96a4772c284d051c47fa00c
Michael Light <mklight06@gmail.com>
2023-01-16 19:33:59 -0700

    continued webpack config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/cjs/react-dom.production.min';
+import {createRoot} from 'react-dom/client';
 import HomePage from '../../src/HomePage';
 
+const element = {
+  <BrowserRouter>
+}
+
 const root = createRoot(document.getElementById('root'));
-root.render(<HomePage />);
+root.render(
+  <React.StrictMode>
+    <HomePage/>
+  </React.StrictMode>
+);
--- frontend/weback.config.js
+++ frontend/weback.config.js
@@ -9,6 +9,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'deploy'),
   },
+  rules: [
+    {
+      test: /\.jsx$|\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env", "@babel/preset-react"]
+      }
+    }
+  ],
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack Output',
