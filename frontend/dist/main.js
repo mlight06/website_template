8a11a087872b65b01b687c5199e57419ca97e3b6
Michael Light <mklight06@gmail.com>
2023-01-19 20:33:09 -0700

    continued config for webpack

--- frontend/dist/main.js
+++ frontend/dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("throw new Error(\"Module parse failed: Unexpected token (6:2)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| \\n| const element = {\\n>   <BrowserRouter>\\n| }\\n| \");\n\n//# sourceURL=webpack://website_template/./src/index.js?");
+eval("throw new Error(\"Module parse failed: Unexpected token (12:4)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| root.render(\\n|   // <React.StrictMode>\\n>     <HomePage/>\\n|   {/* </React.StrictMode> */}\\n| );\");\n\n//# sourceURL=webpack://website_template/./src/index.js?");
 
 /***/ })
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import HomePage from '../../src/HomePage';
 
-const element = {
-  <BrowserRouter>
-}
+// const element = {
+//   <BrowserRouter>
+// }
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <HomePage/>
-  </React.StrictMode>
+  {/* </React.StrictMode> */}
 );
